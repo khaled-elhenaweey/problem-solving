b3ff622ea709e9b7afd59b45ad0a38a17fb17b62
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2025-11-02 11:45:13 +0200

    Add solution - CodeHub

---
+++ 7-kyu/sum-of-the-first-nth-term-of-series/sum-of-the-first-nth-term-of-series.js
@@ -0,0 +1,14 @@
+​
+function SeriesSum(n) { 
+    let zero = 0;
+    let fraction = 0; 
+    let denominator = 1;
+​
+    if (n === zero) {↔}  
+​
+    for (let i = 1; i < n; i++) {↔} 
+​
+    let number = 1 + fraction;
+    return number.toFixed(2);
+ }
+​
