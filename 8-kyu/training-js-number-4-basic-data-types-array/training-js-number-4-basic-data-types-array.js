47199f5ceff3df00885acf36f3dddbf80746d917
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2025-11-05 12:27:06 +0200

    Add solution - CodeHub

---
+++ 8-kyu/training-js-number-4-basic-data-types-array/training-js-number-4-basic-data-types-array.js
@@ -0,0 +1,12 @@
+function getLength(arr){↔}
+function getFirst(arr){↔}
+function getLast(arr){↔}
+function pushElement(arr){
+  var el=1;
+   arr.push(el)
+  return arr
+}
+function popElement(arr){
+  arr.pop()  
+  return arr
+}
