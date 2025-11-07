47038ed17cea0cc0dd0d9c8620ea8cb97f0d65af
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2025-11-07 13:04:18 +0200

    Update solution - CodeHub

--- 8-kyu/string-repeat/string-repeat.js
+++ 8-kyu/string-repeat/string-repeat.js
@@ -1,10 +1,10 @@
 function repeatStr (n, s) {
-    let str = s;
-    if(n > 0){
-       for(let i = 1; i < n; i++){
-           str += s
-       }
-          return str;
+    let repeatStr = s;
+  if(n > 0){
+    for(let i = 1; i<n; i++){
+        repeatStr += s
     }
-    return '';
+    return repeatStr;
+  }
+     return '';
 }
