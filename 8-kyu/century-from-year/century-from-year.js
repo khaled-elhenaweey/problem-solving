08d4ae19db784cd0d89128714f3ee4d903c7cba3
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2025-11-12 13:40:21 +0200

    Update solution - CodeHub

--- 8-kyu/century-from-year/century-from-year.js
+++ 8-kyu/century-from-year/century-from-year.js
@@ -1,11 +1,3 @@
 function century(year) {
- let yearStr = String(year);
- let yearLastNumber = Number(yearStr.slice(-1));
-    if (yearStr.length > 3){
-       return yearLastNumber >= 1 ? Number(yearStr.slice(0,2)) + 1 : Number(yearStr.slice(0,2));
-    } else if (yearStr.length == 3){
-      return yearLastNumber >= 1 ? Number(yearStr.slice(0,1)) + 1 : Number(yearStr.slice(0,1));
-    }else if (yearStr.length <= 2){
-       return 1;
-    }
+  return Math.ceil(year/100);
 }
