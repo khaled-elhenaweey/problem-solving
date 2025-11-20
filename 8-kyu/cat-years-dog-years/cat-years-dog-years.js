3f3b0e8822e89f7b0edfd1f2237c455753a3a2af
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2025-11-20 23:00:00 +0200

    Add solution - CodeHub

---
+++ 8-kyu/cat-years-dog-years/cat-years-dog-years.js
@@ -0,0 +1,18 @@
+var humanYearsCatYearsDogYears = function(humanYears) {
+  let dogYears = 0;
+  let catYears = 0;
+  for (let i=1; i <= humanYears; i++){
+    if (i == 1){
+      dogYears += 15
+      catYears += 15
+    }else if (i == 2){
+      dogYears += 9
+      catYears += 9
+    }else{
+      dogYears += 5
+      catYears += 4
+    }
+  }
+  return [humanYears,catYears,dogYears];
+}
+​
