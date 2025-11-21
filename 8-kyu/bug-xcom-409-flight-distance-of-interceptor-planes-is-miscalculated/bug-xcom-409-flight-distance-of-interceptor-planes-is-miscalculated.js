1ce3b32f46da7f08714ec2e45d46c49c0146436f
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2025-11-22 00:20:59 +0200

    Add solution - CodeHub

---
+++ 8-kyu/bug-xcom-409-flight-distance-of-interceptor-planes-is-miscalculated/bug-xcom-409-flight-distance-of-interceptor-planes-is-miscalculated.js
@@ -0,0 +1,12 @@
+// speed of aircrafts is given in *knots*
+// travelTime is in *minutes*
+// travel distance should be returned in *kilometers*
+function travelDistance(avgSpeed, travelTime) {
+  const KM_PER_MILE = 1.852;
+  const travelHours = travelTime / 60;
+  const travelMiles = avgSpeed * travelHours;
+  const travelKms   = travelMiles * KM_PER_MILE;
+  
+  return travelKms;
+}
+​
